53fdc4336fe140cc71b31b517f15f977fee1b24b
hezhomes6-png <hezhomes6@gmail.com>
2026-03-29 18:35:45 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+input.onGesture(Gesture.Shake, function () {
+    for (let index = 0; index < 4; index++) {
+    	
+    }
+})
+for (let index = 0; index < 4; index++) {
+	
+}
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
+basic.forever(function () {
+    led.plot(0, 0)
+})
+control.inBackground(function () {
+	
+})
